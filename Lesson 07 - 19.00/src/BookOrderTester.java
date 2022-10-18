1aa931111d0f47439f7ee6ab6b6adc45e4f946c2
ariefouren <aporan@ono.ac.il>
2022-10-18 18:48:14 +0300

    XX

---
+++ Lesson 07 - 19.00/src/BookOrderTester.java
@@ -0,0 +1,24 @@
+/*
+ * BookOrderTester.java
+ */
+public class BookOrderTester {
+
+	public static void main(String[] args) {
+		
+		Book book1 = new Book("Java Concepts", "Cay S. Horstmann", 400);
+		Book book2 = new Book("Thinking in Java", "Bruce Eckel", 1500);
+		Book book3 = new Book("Java, How to Program", "P. Deitel, H. Deitel", 1200);
+		Book book4 = new Book("Effective Java", "Joshua Bloch", 500);
+		Book book5 = new Book("Java: A Beginner’s Guide", "Herbert Schildt", 900);
+		
+		BookOrder javaBooks = new BookOrder(12407, "24-Aug-2021");
+		javaBooks.addBook(book1);
+		javaBooks.addBook(book2);
+		javaBooks.addBook(book3);
+		javaBooks.addBook(book4);
+		javaBooks.addBook(book5);
+		
+		System.out.print(javaBooks.toString());
+
+	}
+}
